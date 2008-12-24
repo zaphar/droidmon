f1d59d61dba4936385e62e867a6d73ab43925fbb
jwall <jeremy@marzhillstudios.com>
2008-12-23 19:20:04 -0600

    a whole bunch more data being retrieved

--- DroidMon/src/com/marzhillstudios/android/monitor/DroidMon.java
+++ DroidMon/src/com/marzhillstudios/android/monitor/DroidMon.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.StatFs;
 import android.widget.TextView;
 
 public class DroidMon extends Activity {
-    /** Called when the activity is first created. */
+    
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +31,34 @@ public class DroidMon extends Activity {
    		 		;
         }
         catch (SecurityException e) {
-        	content = "Exception: ["+e.toString()+"] encountered while getting process and task info";
+        	content += "Exception: ["+e.toString()+"] encountered while getting process and task info";
 		 		;
         }
-        
+        try {
+	        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(CONNECTIVITY_SERVICE);
+	        NetworkInfo[] ni = cm.getAllNetworkInfo();
+	        content += "Networks: "+ni.length+"\n";
+	        for (int n=0; n<ni.length; n++) {
+	        	content +="Network "+ni[n].getTypeName() + " is "+ ni[n].getDetailedState().name() + "\n";
+	        }
+	        StatFs fs = new StatFs("/data");
+	        content += "Disk Free blocks: " + fs.getFreeBlocks() + "\n";
+	        content += "Disk Total Blocks: " + fs.getBlockCount() + "\n";
+	        
+	        LocationManager lm = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+	        List<String> providers = lm.getAllProviders();
+	        for (String p : providers) {
+	        	Location loc = lm.getLastKnownLocation(p);
+	        	double altitude = loc.getAltitude();
+	        	double latitude = loc.getLatitude();
+	        	double longitude = loc.getLongitude();
+	        	float bearing = loc.getBearing();
+	        	content += "Provider "+p+"has alt:"+altitude+", lat:"+latitude+", long:"+longitude+" at bearing:"+bearing;
+	        }
+        }
+        catch (Exception e) {
+        	content += "Encountered exception: "+e;
+        }
         //Render our text view
         tv.setText(content);
         setContentView(tv);
